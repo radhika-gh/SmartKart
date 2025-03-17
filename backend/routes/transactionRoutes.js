712447321a2d6f3b08c114910e42e8aba04785ed
radhika-gh <agrawalradhika120@gmail.com>
2025-03-17 18:43:12 +0530

    new

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Transaction = require("backend/models/Transaction");
+const Transaction = require("../models/Transaction");
 const Cart = require("../models/Cart");
 const router = express.Router();
 
