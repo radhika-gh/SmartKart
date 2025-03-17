6481bf7a3a25282397e6a73662c9c7f4b51cd490
Swayam <125536817+Swayam0407@users.noreply.github.com>
2025-03-17 18:19:24 +0530

    changes

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema({
-  cartId: { type: String, required: true, unique: true }, 
+  cartId: { type: String, required: true, unique: true }, // 4-digit unique cart ID
   items: [
     {
       productId: { type: String, required: true },
       name: { type: String, required: true },
       price: { type: Number, required: true },
-      weight: { type: Number, required: true }, 
+      weight: { type: Number, required: true },
       expiryDate: { type: Date },
       quantity: { type: Number, default: 1 },
     },
   ],
   totalPrice: { type: Number, default: 0 },
-  totalWeight: { type: Number, default: 0 }, 
+  totalWeight: { type: Number, default: 0 },
+  active: { type: Boolean, default: false }, 
 });
 
 module.exports = mongoose.model("Cart", CartSchema);
