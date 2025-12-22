139acc247091bda03a32d2e050537c9059ecb9f7
Radhika <agrawalradhika120@gmail.com>
2025-12-23 02:25:39 +0530

    fixing razorpay

--- backend/razorpayconfig.js
+++ backend/razorpayconfig.js
@@ -2,8 +2,8 @@ const Razorpay = require("razorpay");
 require("dotenv").config();
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_ID_KEY,
-  key_secret: process.env.RAZORPAY_SECRET_KEY,
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 module.exports = razorpay;
